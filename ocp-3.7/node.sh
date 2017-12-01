40dbd722ee985f935b98bd4642f8da128bcbffdb
lbroudoux <laurent.broudoux@gmail.com>
2017-12-01 14:01:45 +0100

    Restore everything as it seem to wark when manually done

--- ocp-3.7/node.sh
+++ ocp-3.7/node.sh
@@ -6,36 +6,36 @@ rhn_pass=$2
 rhn_pool=$3
 
 
-#subscription-manager register --username=${rhn_username} --password=${rhn_pass} --force
-#subscription-manager attach --pool=${rhn_pool}
+subscription-manager register --username=${rhn_username} --password=${rhn_pass} --force
+subscription-manager attach --pool=${rhn_pool}
 
-#subscription-manager repos --disable="*"
-#subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.7-rpms" --enable="rhel-7-fast-datapath-rpms"
+subscription-manager repos --disable="*"
+subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.7-rpms" --enable="rhel-7-fast-datapath-rpms"
 
-#sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rh-cloud.repo
-#sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rhui-load-balancers
+sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rh-cloud.repo
+sed -i -e 's/sslverify=1/sslverify=0/' /etc/yum.repos.d/rhui-load-balancers
 
-#yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
-#yum -y update
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+yum -y update
 
-#yum -y install docker-1.12.6
+yum -y install docker-1.12.6
 
-#sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker
+sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker
 
-#cat <<EOF > /etc/sysconfig/docker-storage-setup
-#DEVS=/dev/sdc
-#VG=docker-vg
-#EOF
+cat <<EOF > /etc/sysconfig/docker-storage-setup
+DEVS=/dev/sdc
+VG=docker-vg
+EOF
 
-#docker-storage-setup
-#systemctl enable docker
-#systemctl start docker
+docker-storage-setup
+systemctl enable docker
+systemctl start docker
 
-#yum -y install nfs-utils rpcbind
-#systemctl enable rpcbind
-#systemctl start rpcbind
-#setsebool -P virt_sandbox_use_nfs 1
-#setsebool -P virt_use_nfs 1
+yum -y install nfs-utils rpcbind
+systemctl enable rpcbind
+systemctl start rpcbind
+setsebool -P virt_sandbox_use_nfs 1
+setsebool -P virt_use_nfs 1
 
-#ystemctl stop firewalld
-#systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable firewalld
